9ef5aadcfe08e3553e7c979a47886c09f403a84c
vinitdarad262000@gmail.com <vinitdarad262000@gmail.com>
2025-09-19 00:31:49 +0530

    Added tests

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const ip = require('ip');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    const serverIp = ip.address();
+    return res.json({
+        ping: 'pong',
+        message: serverIp
+    })
+});
+
+app.listen(3000, () => {
+    console.log("Server is running");
+});
